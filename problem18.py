974e11047ca23fc3a25d4d5eb8975b48dd222be5
Aman Agarwal <aman.agarwal97@gmail.com>
2018-01-16 13:43:41 +0000

    Added dynamic programming solution

--- problem18.py
+++ problem18.py
@@ -1,22 +1,21 @@
-list_numbers = []
-with open('project_euler/input18.txt') as file:
-    for line in file:
+arr = []
+with open('input18.txt') as file:
+    for count,line in enumerate(file,1):
+        pass
+    arr = [[0 for j in range(count)] for i in range(count)]
+    file.seek(0,0)
+    for i,line in enumerate(file):
         num_list = line.split(' ')
-        for item in num_list :
-            item = int(item)
-        list_numbers.append(num_list)
+        for j,item in enumerate(num_list) :
+            arr[i][j] = int(item)
 
-max_count = int(list_numbers[0][0])
-print(max_count)
-index = 0
-for i in range(1,len(list_numbers)) :
-    if int(list_numbers[i][index]) > int(list_numbers[i][index+1]) :
-        num = int(list_numbers[i][index])
-    else :
-        num = int(list_numbers[i][index+1])
-        index += 1
-    max_count += num
-    print(num)
+
+for i in range(count-1,0,-1):
+    for j in range(i,0,-1):
+        arr[i-1][j-1] = max(arr[i][j]+arr[i-1][j-1],arr[i][j-1]+arr[i-1][j-1])
+        
+        
+print(arr[0][0])
     
-print(max_count)
+
     
