4f7f9346017a080033b9a1dcea6c3b6aa56716d9
Aman Agarwal <aman.agarwal97@gmail.com>
2018-01-16 12:56:08 +0000

    Added efficient approach

--- problem1.py
+++ problem1.py
@@ -1,7 +1,23 @@
 
-sum=0
-for i in range(0,1000):
-    if i%5 == 0 or i%3 == 0 :
-        sum += i
+
+limit = 1000000
+
+def SimpleApproach():
     
-print(sum)
+    print(sum([i for i in range(limit) if i%5==0 or i%3==0]))
+
+def EfficientApproach():
+    print(SumDivisibleBy(3)+SumDivisibleBy(5)-SumDivisibleBy(15))
+
+
+def SumDivisibleBy(n):
+    p = (limit-1) // n
+    sum = n*(p*(p+1))/2
+    return sum
+    
+if __name__ == '__main__':
+    EfficientApproach()
+    SimpleApproach()
+    
+
+    
